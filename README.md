9d8cbc0b8de8fcf0b3fb9256c1a87c9c716f6289
Isabelle Caspersson <isabellecaspersson@MacBook-Pro.local>
2020-05-01 13:05:45 +0200

    readme

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # yogasyster
 
-ABOUT
 Yogasyster is a homepage for a customer that is newly educated as a yoga teacher. The customer had specifications on what links to have in the navbar, what pictures to use and that it should be earthy colors with some red hints. When I started to make the site I realized that I needed som sketches for layout which I also added.
 
 The project started as a "hackday" at School of applied technology. I am still adding features and style to it and the site will be ready to deploy in june 2020.
 
 The site has frontend and a backend with Nodemailer. For the backend I have use nodemaile to send emails direct to a existing (fake)mail. When the page launch I will change the email and where to store sensitive data.
 
-TECH STACK
+<img src=".client/src/components/pics/preview.jpg" alt="preview" width="600"/>
+
+## 👨‍💻 Tech stack
+
 React (Functional and class components, Hooks, Routes)
 Styled Components
 Express
 Node.js
 Nodemailer (it's a module for Node.js applications to allow email sending.)
 
-USE THE SITE
+## 🧪 Testing
+
 Clone the project.
 
 - Go to client "npm start"
--- routes/contact.js
+++ routes/contact.js
@@ -1,50 +1,50 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var nodemailer = require("nodemailer");
-var cors = require("cors");
+var nodemailer = require('nodemailer');
+//var cors = require("cors");
 // const creds = require("../config");
 
 var smtpTrans = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
   auth: {
-    user: "ingafakesson@gmail.com",
-    pass: "epicenter"
-  }
+    user: 'ingafakesson@gmail.com',
+    pass: 'epicenter',
+  },
 });
 
 smtpTrans.verify((error, success) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Server is ready to take messages");
+    console.log('Server is ready to take messages');
   }
 });
 
-router.post("/send", (req, res, next) => {
+router.post('/send', (req, res, next) => {
   //console.log("****INSIDE SEND ROUTER LOGGIN BODY", req.body);
   var name = req.body.data.name;
   var email = req.body.data.email;
   var message = req.body.data.message;
   var content = `name: ${name} \n email: ${email} \n message: ${message} `;
-  console.log("*****LOGGING CONTENT", content);
+  console.log('*****LOGGING CONTENT', content);
 
   var mailOptions = {
     from: req.body.data.email,
     sender: req.body.data.email,
-    to: "ingafakesson@gmail.com",
-    subject: "Email from yogasyster.se",
-    text: req.body.data.message
+    to: 'ingafakesson@gmail.com',
+    subject: 'Email from yogasyster.se',
+    text: req.body.data.message,
   };
 
-  smtpTrans.sendMail(mailOptions, function(error, success) {
+  smtpTrans.sendMail(mailOptions, function (error, success) {
     if (error) {
       res.send({
-        status: "fail"
+        status: 'fail',
       });
       res.end();
     } else {
       res.send({
-        status: "success"
+        status: 'success',
       });
       res.end();
     }

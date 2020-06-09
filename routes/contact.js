b615e5d44baf79adfb367bce68ec4e2b36f11fc2
Isabelle-123 <isacas031@gmail.com>
2020-06-09 14:52:10 +0200

    replyTo works when getting email from page

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var nodemailer = require('nodemailer');
+var express = require('express')
+var router = express.Router()
+var nodemailer = require('nodemailer')
 //var cors = require("cors");
 // const creds = require("../config");
 
@@ -10,45 +10,45 @@ var smtpTrans = nodemailer.createTransport({
     user: 'ingafakesson@gmail.com',
     pass: 'epicenter',
   },
-});
+})
 
 smtpTrans.verify((error, success) => {
   if (error) {
-    console.log(error);
+    console.log(error)
   } else {
-    console.log('Server is ready to take messages');
+    console.log('Server is ready to take messages')
   }
-});
+})
 
 router.post('/send', (req, res, next) => {
   //console.log("****INSIDE SEND ROUTER LOGGIN BODY", req.body);
-  var name = req.body.data.name;
-  var email = req.body.data.email;
-  var message = req.body.data.message;
-  var content = `name: ${name} \n email: ${email} \n message: ${message} `;
-  console.log('*****LOGGING CONTENT', content);
+  var name = req.body.data.name
+  var email = req.body.data.email
+  var message = req.body.data.message
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `
+  console.log('*****LOGGING CONTENT', content)
 
   var mailOptions = {
-    from: req.body.data.email,
-    sender: req.body.data.email,
+    replyTo: req.body.data.email,
+    // sender: req.body.data.email,
     to: 'ingafakesson@gmail.com',
     subject: 'Email from yogasyster.se',
     text: req.body.data.message,
-  };
+  }
 
   smtpTrans.sendMail(mailOptions, function (error, success) {
     if (error) {
       res.send({
         status: 'fail',
-      });
-      res.end();
+      })
+      res.end()
     } else {
       res.send({
         status: 'success',
-      });
-      res.end();
+      })
+      res.end()
     }
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router

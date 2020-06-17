637c6a649fd640cc2053f0c3fd8957429ce9be82
Isabelle-123 <isacas031@gmail.com>
2020-06-17 11:40:14 +0200

    cleaned up, css bug

--- client/src/components/homeFolder/StyleHomeSecondSlide.js
+++ client/src/components/homeFolder/StyleHomeSecondSlide.js
@@ -34,79 +34,59 @@ export const StyleHomeSecondSlide = styled.section`
     width: 290px;
     height: 320px;
     border-radius: 5px;
-  
+    margin-bottom: 3%;
   }
+
   ${'' /*slide 2 towards tablet */}
   @media  screen and (max-width: 970px) {
-    
     .card {
       width: auto;
     }
 
     .link {
-      flex-direction: column;
-      align-items: center;
       font-size: 1.5em;
       color:red;
     }
 
     .link > img { 
-      width: 25vw;
-      height: 25vh;
-      margin-right: 5%;
-      margin-left: 5%;
+      width: 27vw;
+      height: 30vh;
     }
   }
 
   ${'' /* slide 2 tablet */}
+  @media only screen and (max-width: 790px) {
+    .card-container {
+      align-content: center;
+    }
+    
+    .card {
+      width: 100%;
+    }
 
-@media only screen and (max-width: 790px) {
-  
-  .card-container {
-   justify-content: space-evenly;
-  }
-  
-  .card {
-    width: 100%;
-  }
+    .link {
+      width: 100%;
+      flex-direction: row;
+    }
 
-  .link {
-    width: 100%;
-    color:green;
-    flex-direction: row;
+    .link > img { 
+      height: 240px;
+      width: 200px;
+      margin: 0 3% 0 10%;
+    }
   }
-
-  .link > img { 
-    height: 240px;
-    width: 200px;
-  }
-}
 
 
 ${'' /* slide 2 mobile view */}
 
   @media only screen and (max-width: 540px) {
     .card-container {
-     
+      height: 80vh;   
     }
     
-    .card {
-     justify-content: center;
-      width: 100%;
-    }
-
-    .link {
-      flex-direction: row;
-      width: 100%;
-      color:blue;
-    }
-
     .link > img { {
       height: 140px;
       width: 110px;
-      margin-right: 5%;
-      margin-left: 5%;
     }
   }
-
 `

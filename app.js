40e6c53d358aed8061341f43a8c1da589f3703e3
Isabelle-123 <isacas031@gmail.com>
2020-06-21 16:08:15 +0200

    added static.json to make refresh page to work

--- app.js
+++ app.js
@@ -24,18 +24,18 @@ app.use('/contact', usersRouter)
 
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 
-app.use(function (req, res, next) {
-  next(createError(404))
-})
+// app.use(function (req, res, next) {
+//   next(createError(404))
+// })
 
-//error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-  res.status(err.status || 500)
-  res.send('error')
-})
+// //error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message
+//   res.locals.error = req.app.get('env') === 'development' ? err : {}
+//   res.status(err.status || 500)
+//   res.send('error')
+// })
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))

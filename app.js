d8d2d49721189902deb082e00c38ab3c9abde9cb
Isabelle-123 <isacas031@gmail.com>
2020-06-21 11:04:07 +0200

    saving

--- app.js
+++ app.js
@@ -31,18 +31,18 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-// app.use(function (req, res, next) {
-//   next(createError(404))
-// })
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
-// error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message
-//   res.locals.error = req.app.get('env') === 'development' ? err : {}
-//   res.status(err.status || 500)
-//   res.send('error')
-// })
+//error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.status(err.status || 500)
+  res.send('error')
+})
 
 app.listen(PORT)
 console.log(`App listening on ${PORT}`)

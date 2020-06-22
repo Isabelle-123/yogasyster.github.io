c9ded24986c719e5a9f27c4624e85124b76d1aee
Isabelle-123 <isacas031@gmail.com>
2020-06-22 23:33:50 +0200

    horizontal mobile view ok

--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,8 @@ const App = () => {
         <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
           <Route path='/classes' component={Classes} />
+          <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
           <Route component={My404RedirectToNotFound} />
         </Switch>

e77420bc77d25181877b81198614961f79f1f4b1
Isabelle-123 <isacas031@gmail.com>
2020-06-30 10:39:44 +0200

    updated fonts

--- client/src/globalStyle.js
+++ client/src/globalStyle.js
@@ -1,10 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
-import FontBellota from './components/assets/fonts/Bellota-Regular.ttf'
-import FontBellota2 from './components/assets/fonts/Bellota-Regular.woff'
-import FontBellota3 from './components/assets/fonts/Bellota-Regular.woff2'
-import FontZeyada from './components/assets/fonts/Zeyada-Regular.ttf'
-import FontZeyada2 from './components/assets/fonts/Zeyada-Regular.woff'
-import FontZeyada3 from './components/assets/fonts/Zeyada-Regular.woff2'
+import FontBellota from './components/assets/fonts/Bellota-Regular.woff'
+import FontBellota2 from './components/assets/fonts/Bellota-Regular.woff2'
+import FontZeyada from './components/assets/fonts/Zeyada-Regular.woff'
+import FontZeyada2 from './components/assets/fonts/Zeyada-Regular.woff2'
 
 const GlobalStyle = createGlobalStyle`
   html, body {
@@ -17,15 +15,24 @@ const GlobalStyle = createGlobalStyle`
     font-family: Helvetica, Arial, sans-serif;
   }
 
+  
   @font-face {
     font-family: 'Bellota';
-    src: local('Bellota'), local('Bellota'), url(${FontZeyada}) format('ttf'),
-      url(${FontBellota2}) format('woff'), url(${FontBellota3}) format('woff2');
+    font-weight: 400;  
+    font-style: normal;  
+    font-display: swap; /* Read next point */  
+    unicode-range: U+000-5FF; /* Download only latin glyphs */  
+    src: local('Bellota'), url(${FontBellota2}) format('woff2'), url(${FontBellota}) format('woff');
   }
+
   @font-face {
     font-family: 'Zeyada';
-    src: local('Zeyada'), local('Zeyada'), url(${FontBellota}) format('ttf'),
-      url(${FontZeyada2}) format('woff'), url(${FontZeyada3}) format('woff2');
+    font-weight: 400;  
+    font-style: normal;  
+    font-display: swap; /* Read next point */  
+    unicode-range: U+000-5FF; /* Download only latin glyphs */  
+    src: local('Zeyada'), url(${FontZeyada2}) format('woff2'),
+      url(${FontZeyada}) format('woff');
   }
 `
 
